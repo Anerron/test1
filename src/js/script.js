5d4656d97cca2f6b6dd671bf8ec2019a88531eb4
Arthur <arturkendys87@gmail.com>
2025-10-26 01:02:17 +0300

    Added adaptive section

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,23 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid-full.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid-full.svg"></button>',
     });
+
+    $('.contacts__forms').validate({
+        rules: {
+            name: "required",
+            message: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Please specify your name",
+            message: "Please enter your message",
+            email: {
+                required: "We need your email address to contact you",
+                email: "Your email address must be in the format of name@domain.com"
+            }
+        }
+    });
 });
